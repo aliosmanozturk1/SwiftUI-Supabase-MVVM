dd712e5f488b2bc10d7c8993c444ffc555d743a6
Ali Osman Öztürk <github@aliosmanozturk.me>
2025-05-19 00:58:04 +0300

    Create Blank Project

---
+++ SupabaseMVVM/SupabaseMVVM/SupabaseMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupabaseMVVMApp.swift
+//  SupabaseMVVM
+//
+//  Created by Ali Osman Öztürk on 19.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct SupabaseMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
